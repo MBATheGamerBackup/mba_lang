5fbe3510d2a62f07030d2fb72fa6abecd63ea3dc
MBATheGamer <mbathegamer@gmail.com>
2023-11-23 16:04:53 +0100

    Add expression statement parser + add a test

---
+++ ast/ast_test.go
@@ -0,0 +1,39 @@
+package ast_test
+
+import (
+	"testing"
+
+	"github.com/MBATheGamer/mba_lang/ast"
+	"github.com/MBATheGamer/mba_lang/token"
+)
+
+func TestString(t *testing.T) {
+	var program = &ast.Program{
+		Statements: []ast.Statement{
+			&ast.LetStatement{
+				Token: token.Token{
+					Type:    token.LET,
+					Literal: "let",
+				},
+				Name: &ast.Identifier{
+					Token: token.Token{
+						Type:    token.IDENT,
+						Literal: "myVar",
+					},
+					Value: "myVar",
+				},
+				Value: &ast.Identifier{
+					Token: token.Token{
+						Type:    token.IDENT,
+						Literal: "anotherVar",
+					},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+
+	if program.String() != "let myVar = anotherVar;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
--- parser/parser.go
+++ parser/parser.go
@@ -8,16 +8,32 @@ import (
 	"github.com/MBATheGamer/mba_lang/token"
 )
 
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(ast.Expression) ast.Expression
+)
+
 type Parser struct {
 	l         *lexer.Lexer
 	errors    []string
 	curToken  token.Token
 	peekToken token.Token
+
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
 }
 
 func (p *Parser) nextToken() {
 	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
 }
 
 func New(l *lexer.Lexer) *Parser {
